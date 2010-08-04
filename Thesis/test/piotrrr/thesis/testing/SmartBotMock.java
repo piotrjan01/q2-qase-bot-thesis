c888ddb1789af79f4bad5f8ee3d7829b30e96897
gwizdek <gwizdek@963b5f2f-6fc6-87b5-0d58-0b3443685815>
2010-08-04 22:28:28 +0000

    refactoring....

---
+++ Thesis/test/piotrrr/thesis/testing/SmartBotMock.java
@@ -0,0 +1,41 @@
+package piotrrr.thesis.testing;
+
+import piotrrr.thesis.bots.referencebot.ReferenceBot;
+import piotrrr.thesis.bots.tuning.NavConfig;
+
+public class SmartBotMock extends ReferenceBot {
+
+	public SmartBotMock() {
+		super("MOCK", "none");
+		nConfig = new NavConfig();
+	}
+	
+	float health = 20;
+	
+	float armor = 30;
+	
+	@Override
+	public float getBotHealth() {
+		return health;
+	}
+	
+	@Override
+	public float getBotArmor() {
+		return armor;
+	}
+
+	public void setHealth(float health) {
+		this.health = health;
+	}
+
+	public void setArmor(float armor) {
+		this.armor = armor;
+	}
+	
+	@Override
+	public float getAmmunitionState(int gunIndex) {
+		// TODO Auto-generated method stub
+		return 0.7f;
+	}
+
+}
