f3499304ec538390912028e9ddd2b93f96df557e
gwizdek <gwizdek@963b5f2f-6fc6-87b5-0d58-0b3443685815>
2010-09-20 17:58:25 +0000

    :(

--- trunk/Thesis/test/piotrrr/thesis/testing/RankingTests.java
+++ trunk/Thesis/test/piotrrr/thesis/testing/RankingTests.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import piotrrr.thesis.bots.referencebot.ReferenceBot;
-import piotrrr.thesis.bots.referencebot.ReferenceBotEntityRanking;
+import piotrrr.thesis.common.navigation.FuzzyEntityRanking;
 import piotrrr.thesis.tools.Dbg;
 import soc.qase.state.Entity;
 
@@ -29,38 +29,38 @@ public class RankingTests {
 
 	@Test
 	public void testGetBotHealthDeficiency() {
-		float hd = ReferenceBotEntityRanking.getBotHealthDeficiency(bot, 0);
+		float hd = FuzzyEntityRanking.getBotHealthDeficiency(bot, 0);
 		float exp = 1 - bot.health/100f;
 		assertTrue( hd == exp);
 		
-		hd = ReferenceBotEntityRanking.getBotHealthDeficiency(bot, 20);
+		hd = FuzzyEntityRanking.getBotHealthDeficiency(bot, 20);
 		exp = 1 - (bot.health+20)/100f;
 		assertTrue( hd == exp);
 	}
 
 	@Test
 	public void testGetBotArmorDeficiency() {
-		float hd = ReferenceBotEntityRanking.getBotArmorDeficiency(bot, 0);
-		float exp = 1 - bot.armor/ReferenceBot.maxArmor;
+		float hd = FuzzyEntityRanking.getBotArmorDeficiency(bot, 0);
+		float exp = 1 - bot.armor/ReferenceBot.recommendedArmorLevel;
 		assertTrue( hd == exp);
 		
-		hd = ReferenceBotEntityRanking.getBotArmorDeficiency(bot, 50);
-		exp = 1 - (bot.armor+50)/ReferenceBot.maxArmor;
+		hd = FuzzyEntityRanking.getBotArmorDeficiency(bot, 50);
+		exp = 1 - (bot.armor+50)/ReferenceBot.recommendedArmorLevel;
 		assertTrue( hd == exp);
 	}
 
 	@Test
 	public void testGetBotWeaponDeficiency() {
-		float rv = ReferenceBotEntityRanking.getBotWeaponDeficiency(bot, 0);
+		float rv = FuzzyEntityRanking.getBotWeaponDeficiency(bot, 0);
 		float ev = 1;
 		assertTrue( rv == ev);
 		
 		
-		rv = ReferenceBotEntityRanking.getBotWeaponDeficiency(bot, 14);
+		rv = FuzzyEntityRanking.getBotWeaponDeficiency(bot, 14);
 		ev = 0.822f;
 		assertTrue( floatsEqual(rv, ev));
 		
-		rv = ReferenceBotEntityRanking.getBotWeaponDeficiency(bot, 16);
+		rv = FuzzyEntityRanking.getBotWeaponDeficiency(bot, 16);
 		ev = 0.6f;
 		assertTrue( rv == ev);
 		
@@ -68,15 +68,15 @@ public class RankingTests {
 
 	@Test
 	public void testGetBotAmmoDeficiency() {
-		float rv = ReferenceBotEntityRanking.getBotAmmoDeficiency(bot, 0);
+		float rv = FuzzyEntityRanking.getBotAmmoDeficiency(bot, 0);
 		float ev = 1;
 		assertTrue( rv == ev);
 		
-		rv = ReferenceBotEntityRanking.getBotAmmoDeficiency(bot, 22);
+		rv = FuzzyEntityRanking.getBotAmmoDeficiency(bot, 22);
 		ev = 0.8568182f;
 		assertTrue( floatsEqual(rv, ev));
 		
-		rv = ReferenceBotEntityRanking.getBotAmmoDeficiency(bot, 21);
+		rv = FuzzyEntityRanking.getBotAmmoDeficiency(bot, 21);
 		ev = 0.936f;
 		assertTrue( floatsEqual(rv, ev));
 	}
@@ -85,7 +85,7 @@ public class RankingTests {
 	public void testGetItemHealthBenefit() {
 		EntityMock e = new EntityMock(Entity.CAT_ITEMS, Entity.TYPE_HEALTH, Entity.SUBTYPE_LARGE);
 		
-		float rv = ReferenceBotEntityRanking.getItemHealthBenefit(bot, e);
+		float rv = FuzzyEntityRanking.getItemHealthBenefit(bot, e);
 //		Dbg.prn(rv);
 		float ev = 0.199f;
 		assertTrue( floatsEqual(rv, ev));
@@ -95,19 +95,19 @@ public class RankingTests {
 	@Test
 	public void testGetItemArmorBenefit() {
 		EntityMock e = new EntityMock(Entity.CAT_ITEMS, Entity.TYPE_ARMOR, Entity.SUBTYPE_COMBATARMOR);
-		float rv = ReferenceBotEntityRanking.getItemArmorBenefit(bot, e);
+		float rv = FuzzyEntityRanking.getItemArmorBenefit(bot, e);
 //		Dbg.prn(rv);
 		float ev = 0.5f;
 		assertTrue( floatsEqual(rv, ev) );
 		
 		e = new EntityMock(Entity.CAT_ITEMS, Entity.TYPE_ARMOR, Entity.SUBTYPE_ARMORSHARD);
-		rv = ReferenceBotEntityRanking.getItemArmorBenefit(bot, e);
+		rv = FuzzyEntityRanking.getItemArmorBenefit(bot, e);
 //		Dbg.prn(rv);
 		ev = 0.009999f;
 		assertTrue( floatsEqual(rv, ev) );
 		
 		e = new EntityMock(Entity.CAT_ITEMS, Entity.TYPE_CHAINGUN, null);
-		rv = ReferenceBotEntityRanking.getItemArmorBenefit(bot, e);
+		rv = FuzzyEntityRanking.getItemArmorBenefit(bot, e);
 //		Dbg.prn(rv);
 		ev = 0.0f;
 		assertTrue( floatsEqual(rv, ev) );
@@ -117,7 +117,7 @@ public class RankingTests {
 	@Test
 	public void testGetItemWeaponBenefit() {
 		EntityMock e = new EntityMock(Entity.CAT_ITEMS, Entity.TYPE_RAILGUN, null);
-		float rv = ReferenceBotEntityRanking.getItemWeaponBenefit(bot, e);
+		float rv = FuzzyEntityRanking.getItemWeaponBenefit(bot, e);
 //		Dbg.prn(rv);
 		float ev = 0.3999f;
 		assertTrue( floatsEqual(rv, ev) );
@@ -128,7 +128,7 @@ public class RankingTests {
 	@Test
 	public void testGetItemAmmoBenefit() {
 		EntityMock e = new EntityMock(Entity.CAT_ITEMS, Entity.TYPE_AMMO, Entity.SUBTYPE_BULLETS);
-		float rv = ReferenceBotEntityRanking.getItemAmmoBenefit(bot, e);
+		float rv = FuzzyEntityRanking.getItemAmmoBenefit(bot, e);
 		Dbg.prn(rv);
 		float ev = 0.1f;
 		assertTrue( floatsEqual(rv, ev) );
