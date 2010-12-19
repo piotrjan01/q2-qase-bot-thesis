92a0129f1df33e8bc88005832abba47a032b5331
gwizdek <gwizdek@963b5f2f-6fc6-87b5-0d58-0b3443685815>
2010-12-19 21:26:59 +0000

    reference bot and learnbot united

--- trunk/Thesis/src/piotrrr/thesis/bots/tuning/NavConfig.java
+++ trunk/Thesis/src/piotrrr/thesis/bots/tuning/NavConfig.java
@@ -5,36 +5,11 @@ import java.lang.reflect.Field;
 
 public class NavConfig implements Serializable {
 
-    /**
-     * What defitiency we agree to when we attack the enemy
-     */
-    public static float aggressiveness = 0.5f;
-    /**
-     * The recommended ammount of health it is established the bot should have.
-     * It is used to calculate the deficiency of health.
-     */
-    public static final int recommendedHealthLevel = 70;
-    /**
-     * The recommended armor the bot should have.
-     * @see BotBase#recommendedHealthLevel
-     */
-    public static final int recommendedArmorLevel = 10;
-    /**
-     * The recommended firepower the bot should have.
-     * @see BotBase#recommendedHealthLevel
-     */
-    public static final float recommendedWeaponPercent = 0.4f;
-    /**
-     * Recommended ammo percent, the bot should have
-     */
-    public static final float recommendedAmmoPercent = 0.4f;
-
     private static final double stepSize = 0.2;
-
     public OptParam weight_health = new OptParam(0.9f, 0, 1, stepSize);
     public OptParam weight_armor = new OptParam(0.3f, 0, 1, stepSize);
     public OptParam weight_weapon = new OptParam(0.6f, 0, 1, stepSize);
-    public OptParam weight_ammo = new OptParam(0.3f, 0, 1, stepSize);
+    public OptParam weight_ammo = new OptParam(0.5f, 0, 1, stepSize);
     public OptParam weight_distance = new OptParam(0.2f, 0, 1, stepSize);
     public OptParam weight_enemycost = new OptParam(0.5f, 0, 1, stepSize);
     public OptParam weight_aggresiveness = new OptParam(0.7f, 0, 1, stepSize);
